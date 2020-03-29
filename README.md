b30b1e163cafb51453045a53616efb4f642485eb
Paulmicha <paul.michalet@gmail.com>
2020-03-28 22:47:12 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# chouette.net.br
-Chouette - Institut de Français : Main Site
+# Chouette - Institut de Français : Main Site
+
+[![Chouette - Institut de Français logo - www.chouette.net.br](static/logo.svg)](https://www.chouette.net.br/)
+
+Source code is licensed [MIT](LICENSE). The logo, trademark, textual content are
+protected under the Law on Copyright and Neighboring Rights No. 9.610 of
+February 19, 1998 (unless otherwise stated).
---
+++ src/routes/index.svelte
@@ -0,0 +1,44 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Chouette - Institut de Français</title>
+</svelte:head>
+
+<h1>Chouette</h1>
+
+<figure>
+	<img alt='Chouette - Institut de Français' src='/logo.svg'>
+	<figcaption>Chouette - Institut de Français</figcaption>
+</figure>
