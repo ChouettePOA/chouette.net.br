e0a9bdd76bcda87b1a479c5d6713c56fa673603c
Paulmicha <paul.michalet@gmail.com>
2020-04-04 00:45:43 -0300

    wip: begin refacto homepage

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,59 +2,20 @@
 	export let segment;
 </script>
 
-<style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
-</style>
-
-<nav>
-	<ul>
-		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-			 the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
-	</ul>
+<nav class="o-grid o-grid--wrap-until-tablet o-grid--center o-grid--middle o-grid--vgutter-s-until-tablet c-header__menu u-fontB">
+	<div class="first collapsed o-grid__item c-menu-main__item">
+		<a rel=prefetch aria-current='{segment === "sobre-a-escola" ? "page" : undefined}' href="/sobre-a-escola" class="c-menu-main__link u-uppercase">Sobre</a>
+	</div>
+	<div class="leaf o-grid__item c-menu-main__item">
+		<a rel=prefetch aria-current='{segment === "horarios" ? "page" : undefined}' href="/horarios" class="c-menu-main__link u-uppercase">Horários</a>
+	</div>
+	<div class="leaf o-grid__item c-menu-main__item">
+		<a rel=prefetch aria-current='{segment === "valores-2020" ? "page" : undefined}' href="/valores-2020" class="c-menu-main__link u-uppercase">Preços</a>
+	</div>
+	<div class="leaf o-grid__item c-menu-main__item">
+		<a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href="/blog" class="c-menu-main__link u-uppercase">Blog</a>
+	</div>
+	<div class="last leaf o-grid__item c-menu-main__item">
+		<a rel=prefetch aria-current='{segment === "aulas-online" ? "page" : undefined}' href="/aulas-online" class="c-menu-main__link u-uppercase">Aulas online</a>
+	</div>
 </nav>
