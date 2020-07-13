1a0dd38c3d3644882d66b4653c2299e75b9ac0d8
Paulmicha <paul.michalet@gmail.com>
2020-07-13 12:03:57 -0300

    test: WatchedBox element queries (abandoned)

--- src/components/content/Lede.svelte
+++ src/components/content/Lede.svelte
@@ -1,10 +1,13 @@
 <script>
-import Text from '../text/Text.svelte';
-export let text = '';
+	import Text from '../text/Text.svelte';
+	import WatchedBox from '../WatchedBox.svelte';
+	export let text = '';
 </script>
 
-<div class="m-v--xl p-percent-h">
-	<Text {text}
-		options={{ "dropcap":true, "dropcap_variant":"u-dropcap--l" }}
-		attr={{ "class":"c-text-block u-fs-m" }} />
-</div>
+<WatchedBox widthBreaks="70ch, 900px" heightBreaks="50vh, 60em">
+	<div class="m-v--xl p-percent-h">
+		<Text {text}
+			options={{ "dropcap":true, "dropcap_variant":"u-dropcap--l" }}
+			attr={{ "class":"c-text-block u-fs-m" }} />
+	</div>
+</WatchedBox>
