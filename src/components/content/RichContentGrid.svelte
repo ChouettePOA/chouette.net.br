ec17aab431be8f61359ff1448252602c4766d09d
Paulmicha <paul.michalet@gmail.com>
2020-07-14 20:41:59 -0300

    feat: layout RichContentGrid desktop

--- src/components/content/RichContentGrid.svelte
+++ src/components/content/RichContentGrid.svelte
@@ -14,28 +14,33 @@
 	.container {
 		display: grid;
 		gap: 1rem;
-		place-items: stretch;
 		grid-template-columns: auto 30rem 30rem auto;
 		grid-template-rows: auto;
 	}
 	.container > * {
-		grid-row: var(--grid-row);
+		grid-row: var(--row);
 	}
 	.img {
 		grid-column: 2;
 	}
+	.img.img--odd {
+		grid-column: 3;
+	}
 	.body {
 		grid-column: 3;
 	}
+	.body.body--odd {
+		grid-column: 2;
+	}
 </style>
 
-<Responsive w="43rem, 75rem" attr={{ "class":"bg-content" }}>
+<Responsive w="43rem, 75rem" attr={{ "class":"bg-content p-v--l" }}>
 	<div class="container">
 		{#each items as item, i}
-			<div class="img { `img-${i}` }" style="--grid-row:{i + 1}">
+			<div class="img { `img--${i % 2 == 0 ? 'even' : 'odd'}` }" style="--row:{i + 1}">
 				<img src="{ item.image }" alt="{ item.image_alt }" />
 			</div>
-			<div class="body { `body-${i}` }" style="--grid-row:{i + 1}">
+			<div class="body { `body--${i % 2 == 0 ? 'even' : 'odd'}` }" style="--row:{i + 1}">
 				{#if item.title }
 					<h2 class="c-title-1">{ item.title }</h2>
 				{/if}
