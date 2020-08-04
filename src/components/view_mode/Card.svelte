1b8b4eb86a53d8c4b8d6945b7223ab38f15d435b
Paulmicha <paul.michalet@gmail.com>
2020-08-04 20:38:39 -0300

    feat: implement additional lookup subfolder for dynamic components imports preprocessor

---
+++ src/components/view_mode/Card.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Text from '../text/Text.svelte';
+
+	export let title = '';
+	export let path = '';
+	export let description = '';
+	export let published = '';
+</script>
+
+<article class="c-card fx-focus-shadow--ahf fx-scale-up--ahf">
+	<div class="c-card__img">
+		<img alt="{ title }" src="/theme/article-default-500x309.jpg" />
+	</div>
+	<h2 class="c-title-1">
+		<a class="no-b" href="/{ path }">
+			{ title }
+		</a>
+	</h2>
+	<div class="content s-node-content">
+		<Text text={ description } />
+	</div>
+	<div class="o-tgrid o-tgrid--gutter o-tgrid--bottom m-t--s">
+		<small class="o-tgrid__item">
+			<!-- <a href="/tag/chanson" class="no-b">#chanson</a>
+			<a href="/tag/paroles" class="no-b">#paroles</a> -->
+		</small>
+		<small class="o-tgrid__item u-right u-nowrap">
+			{ published }
+		</small>
+	</div>
+</article>
--- src/preprocess.js
+++ src/preprocess.js
@@ -8,8 +8,11 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-const imports_placeholder = '// placeholder://src/preprocess.js';
-const content_placeholder = '<!-- placeholder://src/preprocess.js -->';
+// Determines subfolders of src/components used for generating imports.
+const component_types_lookup = [
+	'content',
+	'view_mode'
+];
 
 /**
  * Recursively gets file paths from given dir.
@@ -34,6 +37,22 @@ const walk = (dir, extension) => {
 }
 
 /**
+ * Returns placeholder by type.
+ */
+const get_placeholder = (placeholder_type, component_type) => {
+	let suffix = '';
+	if (component_type !== 'content') {
+		suffix = '#' + component_type;
+	}
+	switch (placeholder_type) {
+		case 'imports':
+			return `// placeholder://src/preprocess.js${suffix}`;
+		case 'content':
+			return `<!-- placeholder://src/preprocess.js${suffix} -->`;
+	}
+};
+
+/**
  * Transforms components source code at compile time.
  *
  * Caveat : no variables or props are assigned any value. It's just processing
@@ -42,50 +61,53 @@ const walk = (dir, extension) => {
  * See https://svelte.dev/docs#svelte_preprocess
  */
 const markup_alter = (content, filename) => {
-	if (!content.includes(imports_placeholder) || !content.includes(content_placeholder)) {
-		return content;
-	}
-
-	const preprocessed_component = path.parse(filename).name;
-
-	let n = 0;
-	let component = '';
-	let generated_op = '';
-	let generated_imports = '';
-	let generated_contents = '';
-	let relative_path_prefix = '';
-
-	walk('src/components/content', '.svelte').map((file_path) => {
-		component = path.parse(file_path).name;
-
-		// Do not include a component inside itself.
-		if (preprocessed_component == component) {
+	component_types_lookup.forEach(component_type => {
+		if (!content.includes(get_placeholder('imports', component_type))
+			|| !content.includes(get_placeholder('content', component_type))) {
 			return;
 		}
 
-		relative_path_prefix = path.relative(path.dirname(filename), path.dirname(path.resolve(file_path)));
-		if (relative_path_prefix === '') {
-			relative_path_prefix = '.';
-		}
-		file_path = file_path.replace('src/components/content/', relative_path_prefix + '/');
+		const preprocessed_component = path.parse(filename).name;
 
-		if (n === 0) {
-			generated_op = '#if';
-		}
-		else {
-			generated_imports += "\n	";
-			generated_op = ':else if';
-		}
-		n++;
+		let n = 0;
+		let component = '';
+		let generated_op = '';
+		let generated_imports = '';
+		let generated_contents = '';
+		let relative_path_prefix = '';
 
-		generated_imports += `import ${component} from '${file_path}';`;
-		generated_contents += `{${generated_op} c === '${component}'}\n		<${component} {...props} />\n	`;
+		walk('src/components/' + component_type, '.svelte').map((file_path) => {
+			component = path.parse(file_path).name;
+
+			// Do not include a component inside itself.
+			if (preprocessed_component == component) {
+				return;
+			}
+
+			relative_path_prefix = path.relative(path.dirname(filename), path.dirname(path.resolve(file_path)));
+			if (relative_path_prefix === '') {
+				relative_path_prefix = '.';
+			}
+			file_path = file_path.replace('src/components/' + component_type + '/', relative_path_prefix + '/');
+
+			if (n === 0) {
+				generated_op = '#if';
+			}
+			else {
+				generated_imports += "\n	";
+				generated_op = ':else if';
+			}
+			n++;
+
+			generated_imports += `import ${component} from '${file_path}';`;
+			generated_contents += `{${generated_op} c === '${component}'}\n		<${component} {...props} />\n	`;
+		});
+
+		generated_contents += '{/if}';
+
+		content = content.replace(get_placeholder('imports', component_type), generated_imports);
+		content = content.replace(get_placeholder('content', component_type), generated_contents);
 	});
-
-	generated_contents += '{/if}';
-
-	content = content.replace(imports_placeholder, generated_imports);
-	content = content.replace(content_placeholder, generated_contents);
 
 	return content;
 };
