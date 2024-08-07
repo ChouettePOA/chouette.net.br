3de158f5b305970044b145fd136583db77f167f7
Paul <paul.michalet@gmail.com>
2024-08-07 01:10:20 -0300

    feat(main-menu): fix active links

---
+++ src/data/main_menu.js
@@ -0,0 +1,24 @@
+export default {
+	links: [
+		{
+			title: 'Página inicial',
+			href: '/',
+		},
+		{
+			title: 'Sobre a escola',
+			href: '/sobre-a-escola/',
+		},
+		{
+			title: 'Modalidades dos cursos',
+			href: '/modalidades-de-cursos/',
+		},
+		{
+			title: 'Metodologia Chouette',
+			href: '/metodologia-chouette/',
+		},
+		{
+			title: 'Chouettes pelo mundo',
+			href: '/chouettes-pelo-mundo/',
+		}
+	]
+};
