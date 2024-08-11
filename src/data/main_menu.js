347e179445fa6652f81c85d2c536627771d02294
Paul <paul.michalet@gmail.com>
2024-08-11 18:57:06 -0300

    feat(main-menu): wip 2

--- src/data/main_menu.js
+++ src/data/main_menu.js
@@ -6,18 +6,22 @@ export default {
 		// },
 		{
 			title: 'Sobre a escola',
+			title_short: 'Sobre',
 			href: '/sobre-a-escola/',
 		},
 		{
 			title: 'Modalidades dos cursos',
+			title_short: 'Cursos',
 			href: '/modalidades-de-cursos/',
 		},
 		{
 			title: 'Metodologia Chouette',
+			title_short: 'Metodologia',
 			href: '/metodologia-chouette/',
 		},
 		{
 			title: 'Chouettes pelo mundo',
+			title_short: 'Depoimentos',
 			href: '/chouettes-pelo-mundo/',
 		}
 	]
